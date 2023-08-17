281f0a5216368c201b7200f7b9e512114f91046f
david berchier <david.berchier@gmail.com>
2023-08-17 16:00:37 +0200

    watchlist no designed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "lists#index"
+  # get 'lists/:id', to: 'lists#show', as: :movie
+  resources :lists do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
